53469dbb33d772bea984eb88910964b0ab7a2749
Debin <luodeb@outlook.com>
2026-04-03 14:10:39 +0800

    refactor: simplify probe_pci_device function by removing IRQ calculation

--- axdriver_virtio/src/lib.rs
+++ axdriver_virtio/src/lib.rs
@@ -70,22 +70,12 @@ pub fn probe_pci_device<H: VirtIoHal, C: ConfigurationAccess>(
     root: &mut PciRoot<C>,
     bdf: DeviceFunction,
     dev_info: &DeviceFunctionInfo,
-) -> Option<(DeviceType, PciTransport, usize)> {
+) -> Option<(DeviceType, PciTransport)> {
     use virtio_drivers::transport::pci::virtio_device_type;
-
-    #[cfg(target_arch = "x86_64")]
-    const PCI_IRQ_BASE: usize = 0x20;
-    #[cfg(target_arch = "riscv64")]
-    const PCI_IRQ_BASE: usize = 0x20;
-    #[cfg(target_arch = "loongarch64")]
-    const PCI_IRQ_BASE: usize = 0x10;
-    #[cfg(target_arch = "aarch64")]
-    const PCI_IRQ_BASE: usize = 0x23;
 
     let dev_type = virtio_device_type(dev_info).and_then(as_dev_type)?;
     let transport = PciTransport::new::<H, C>(root, bdf).ok()?;
-    let irq = PCI_IRQ_BASE + (bdf.device & 3) as usize;
-    Some((dev_type, transport, irq))
+    Some((dev_type, transport))
 }
 
 const fn as_dev_type(t: VirtIoDevType) -> Option<DeviceType> {
